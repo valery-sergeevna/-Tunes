7cc0258c2ade0002d2c8008c6991869dab3ffa19
Valery <lekalera777@gmail.com>
2020-07-27 10:36:07 +0300

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+import { radioPlayerInit } from "./radioPlayer.js";
+import { videoPlayerInit } from "./videoPlayer.js";
+import { musicPlayerInit } from "./musicPlayer.js";
+
+//объявление переменных
+const playerBtn = document.querySelectorAll('.player-btn'),
+    playerBlock = document.querySelectorAll('.player-block'),
+    temp = document.querySelector('.temp');
+
+//деактивация кнопки и плеера
+const deactivationPlayer = () => {
+    temp.style.display = 'none';
+    playerBtn.forEach(item => item.classList.remove('active'));
+    playerBlock.forEach(item => item.classList.remove('active'));
+};
+//открыть блок при нажатии на кнопку
+playerBtn.forEach((btn, i) => {
+    btn.addEventListener('click', () => {
+        deactivationPlayer();
+        btn.classList.add('active');
+        playerBlock[i].classList.add('active');
+
+    });
+});
+
+
+radioPlayerInit();
+videoPlayerInit();
+musicPlayerInit();
